2077242a357c7354e6db4a47fd127f63be4b155e
Jeremy White <jeremy.white@idean.com>
2014-04-17 02:03:53 -0700

    Added license info to project, updated readme

--- src/com/example/connect_sdk_sampler/MainApplication.java
+++ src/com/example/connect_sdk_sampler/MainApplication.java
@@ -1,3 +1,14 @@
+//
+//  Connect SDK Sample App by LG Electronics
+//
+//  To the extent possible under law, the person who associated CC0 with
+//  this sample app has waived all copyright and related or neighboring rights
+//  to the sample app.
+//
+//  You should have received a copy of the CC0 legalcode along with this
+//  work. If not, see http://creativecommons.org/publicdomain/zero/1.0/.
+//
+
 package com.example.connect_sdk_sampler;
 
 import org.apache.http.impl.client.DefaultHttpClient;
