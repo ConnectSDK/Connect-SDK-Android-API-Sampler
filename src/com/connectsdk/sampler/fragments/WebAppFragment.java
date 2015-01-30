d639a1adc5451e5024aeb1cf15243e19eccef23b
Oleksii Frolov <oleksii.frolov@lge.com>
2015-01-29 16:50:08 -0800

    Optimize import in WebAppFragment

--- src/com/connectsdk/sampler/fragments/WebAppFragment.java
+++ src/com/connectsdk/sampler/fragments/WebAppFragment.java
@@ -15,9 +15,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
