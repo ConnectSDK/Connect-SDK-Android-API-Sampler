aba33206de3a0bd3e84383a593672602d7300538
Ashika.Chhabda <Ashika.Chhabda@lge.com>
2015-02-10 16:33:04 -0800

    Changes made in MainActivity.java and MediaPlayerFragment.java for providing object access to IntegrationTest, Also added a new file specific to be used by IntegrationTest

---
+++ src/com/connectsdk/sampler/util/TestResponseObject.java
@@ -0,0 +1,29 @@
+package com.connectsdk.sampler.util;
+
+public class TestResponseObject {
+
+	public boolean isSuccess;	
+	public int httpResponseCode;	
+	public String responseMessage;
+	
+	public TestResponseObject() {
+		super();
+		this.isSuccess = false;
+		this.httpResponseCode = 0;
+		this.responseMessage = "default";
+		
+	}
+	
+	public TestResponseObject(boolean isSuccess, int httpResponseCode, String responseMessage) {
+		super();
+		this.isSuccess = isSuccess;
+		this.httpResponseCode = httpResponseCode;
+		this.responseMessage = responseMessage;
+	}
+
+	
+
+	
+	
+	
+}
